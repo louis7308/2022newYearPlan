4dd25fa8e4c6302d46182c507dbb2112de16f1e7
louis7308 <jswa7308@naver.com>
2022-01-19 21:25:34 +0900

    Google Oauth2 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GoogleStrategy } from './google.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, GoogleStrategy],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,17 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  googleLogin(req) {
+    if(!req.user) {
+      return '구글 정보에 없는 유저입니다.'
+    }
+    return {
+      message: '구글 정보에 존재하는 유저 입니다.',
+      user: req.user
+    }
   }
 }
+
+
+
+
